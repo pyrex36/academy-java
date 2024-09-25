68408f7e5dedd96f2a4f26dd05b7bdc40bd0a47e
pyrex <72110245+pyrex36@users.noreply.github.com>
2024-09-25 14:53:36 -0400

    added new folder called team4_teach_backs

---
+++ src/main/java/team4_teach_backs/ForLoops.java
@@ -0,0 +1,25 @@
+package team4_teach_backs;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <=5; i++) {
+			System.out.println("We the best!!");
+		}
+		
+		for(int i = 0; i <=10; i++) {
+			   System.out.println(i);
+			}
+		
+		int[] numList = {987, 466, 7664, 8564, 8754, 8643};
+				for(int i = 0; i < numList.length; i++) {
+				   System.out.println(numList[i]);
+				}
+	
+	}
+		 
+			   
+
+	
+}
